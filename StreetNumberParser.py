74e9b2cbdd1174601e15b12122e2503694bf9492
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2018-02-20 08:27:09 -0600

    Minor changes
    Working to get the CSV in the proper format.

--- StreetNumberParser.py
+++ StreetNumberParser.py
@@ -2,32 +2,40 @@ import requests
 import json
 import pandas as pd
 
-STARTING_LAT= 44.890774
+#AIzaSyBFMeWnJ83i8-Bb-UUtJD0HgYhWC7Narys  1901 API key
+#AIzaSyCz5ryOD2HZ5B4WhTadfM7Yza54NuMfTBw  personal API key
+key1901 = 'AIzaSyBb1yDgS5mrnimYWcwnLWApbtgRZFZ1h74'
+keyFuyu = 'AIzaSyCz5ryOD2HZ5B4WhTadfM7Yza54NuMfTBw'
+keyBushido = 'AIzaSyB_PSBGppq3vGjYj01OUNwT7XCtE5-S7Ns'
+
+STARTING_LAT= 44.936838
 LAT_INC  = STARTING_LAT
-STARTING_LONG = -93.000847
+STARTING_LONG = -92.959038
 LONG_INC = STARTING_LONG
-ENDING_LAT = 44.993612
-ENDING_LONG = -92.862771
+ENDING_LAT = 44.963421
+ENDING_LONG = -92.934031
+increment = 0
 
 addresses = []
 
 while(LAT_INC <= ENDING_LAT):
 
     while(LONG_INC <= ENDING_LONG):
-        URL_STRING = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(LAT_INC) + "," + str(LONG_INC) + "&key=AIzaSyCz5ryOD2HZ5B4WhTadfM7Yza54NuMfTBw"
+        increment += 1
+        URL_STRING = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" \
+         + str(LAT_INC) + "," + str(LONG_INC) + "&key=" + key1901
         r = requests.get(URL_STRING)
-        print(URL_STRING)
-        if (not isempty(r.json())):
-            ret = r.json()
-            numbers = ret['results'][0]['address_components'][0]['short_name']
-            streetName = ret['results'][0]['address_components'][1]['short_name']
-            addresses.append((str(LAT_INC) + "," + str(LONG_INC), numbers, streetName))
-            print(results)
-        LONG_INC += 0.0004
+        print(increment)
+        ret = r.json()
+        numbers = ret['results'][0]['address_components'][0]['short_name']
+        streetName = ret['results'][0]['address_components'][1]['short_name']
+        addresses.append((str(LAT_INC), str(LONG_INC), numbers, streetName))
+        print(str(LAT_INC) + "," + str(LONG_INC) + " " + numbers + " " + streetName)
+        LONG_INC += 0.0005
 
-    LAT_INC += .0004
+    LAT_INC += .0005
     LONG_INC = STARTING_LONG
 
 df = pd.DataFrame(addresses)
-df.columns = ['GPS Coordinates', 'Address']
-df.to_csv('addresses.csv', index=False)
+df.columns = ['Lat','Long', 'Numbers', 'Name']
+df.to_csv('Woodbury-A1-addresses.csv', index=False)
