536a201de265467e27f2d31c0a1a51ecbc320f0b
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2018-02-20 09:29:49 -0600

    CSV formatting
    Getting the CSV to be formatted in a manner that will make sorting the
    street names and number a little easier.

--- StreetNumberParser.py
+++ StreetNumberParser.py
@@ -8,14 +8,13 @@ key1901 = 'AIzaSyBb1yDgS5mrnimYWcwnLWApbtgRZFZ1h74'
 keyFuyu = 'AIzaSyCz5ryOD2HZ5B4WhTadfM7Yza54NuMfTBw'
 keyBushido = 'AIzaSyB_PSBGppq3vGjYj01OUNwT7XCtE5-S7Ns'
 
-STARTING_LAT= 44.936838
+STARTING_LAT= 44.890929
 LAT_INC  = STARTING_LAT
-STARTING_LONG = -92.959038
+STARTING_LONG = -92.903284
 LONG_INC = STARTING_LONG
-ENDING_LAT = 44.963421
-ENDING_LONG = -92.934031
+ENDING_LAT = 44.890929
+ENDING_LONG = -92.862943
 increment = 0
-
 addresses = []
 
 while(LAT_INC <= ENDING_LAT):
@@ -23,19 +22,26 @@ while(LAT_INC <= ENDING_LAT):
     while(LONG_INC <= ENDING_LONG):
         increment += 1
         URL_STRING = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" \
-         + str(LAT_INC) + "," + str(LONG_INC) + "&key=" + key1901
+         + str(LAT_INC) + "," + str(LONG_INC) + "&key=" + keyBushido
+
         r = requests.get(URL_STRING)
-        print(increment)
         ret = r.json()
         numbers = ret['results'][0]['address_components'][0]['short_name']
         streetName = ret['results'][0]['address_components'][1]['short_name']
-        addresses.append((str(LAT_INC), str(LONG_INC), numbers, streetName))
-        print(str(LAT_INC) + "," + str(LONG_INC) + " " + numbers + " " + streetName)
+
+        startEnd = numbers.split('-')
+        if (len(startEnd) == 1):
+            startEnd.append(startEnd[0])
+
+        print(str(increment) + " " + str(LAT_INC) + "," + str(LONG_INC) + " start:" + str(startEnd[0]) + " end:" + str(startEnd[1]) + " " + streetName)
+        addresses.append((str(LAT_INC), str(LONG_INC), str(startEnd[0]), str(startEnd[1]), streetName))
         LONG_INC += 0.0005
 
     LAT_INC += .0005
     LONG_INC = STARTING_LONG
 
 df = pd.DataFrame(addresses)
-df.columns = ['Lat','Long', 'Numbers', 'Name']
-df.to_csv('Woodbury-A1-addresses.csv', index=False)
+df.columns = ['Lat','Long', 'Starting Numbers', 'Ending Numbers', 'Name']
+df.to_csv('Woodbury-test-addresses.csv', index=False)
+
+print('End')
