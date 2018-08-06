143df1eb610075c871dd36d5e052c530f98bb6d0
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2018-08-06 10:47:34 -0500

    Update

--- querydb.py
+++ querydb.py
@@ -1,18 +1,37 @@
 import pymongo
+import sys
 
-uri = 'mongodb://frog:Treefort@ds251598.mlab.com:51598/streets'
-client = pymongo.MongoClient(uri)
-db = client['streets']
-streets = db['store_1923']
+def main(store_num):
+    uri = 'localhost:27017'
+    client = pymongo.MongoClient(uri)
+    db = client['streets']
+    streets = db[store_num]
 
-# sectors = ["\'E2\\", "\'W1\\", "\'NW1\\", "\'NW2\\", "\'SE2\\", "\'SW2\\", "\'NE1\\", "\'W2\\", "\'SW1\\", "\'NW3\\", "\'SE1\\", "\'E1\\"]
-#
-# for sector in sectors:
-#     jsob = {
-#         "sector": sector
-#     }
-#     streets.delete_many(jsob)
-#
+    zips = streets.update_many(
+        {'city': "Windom Park"},
+        { '$set' :{'city':"Minneapolis"}}
+    )
 
-zips = streets.distinct("zip code")
-print( zips )
+    zips = streets.aggregate([
+        {
+            '$group': {
+                '_id': {'city': '$city'},
+                'count': { '$sum': 1 }
+            }
+        }
+    ]);
+    for city in list(zips):
+        print(city)
+
+
+    # sectors = ["New Brighton"] #"\'E2\\", "\'W1\\", "\'NW1\\", "\'NW2\\", "\'SE2\\", "\'SW2\\", "\'NE1\\", "\'W2\\", "\'SW1\\", "\'NW3\\", "\'SE1\\", "\'E1\\"]
+    #
+    # for sector in sectors:
+    #     jsob = {
+    #         "city": sector
+    #     }
+    #     streets.delete_many(jsob)
+
+if __name__ == "__main__":
+    store_num = 'store_'+str(sys.argv[1])
+    main(store_num)
